75c38555a2f390a495d6fd70177ec9a4ecba8289
wcx0717 <18911376919@163.com>
2021-10-29 18:35:19 +0800

    解决webpack5 兼容问题

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 // @ts-nocheck
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
 const nextTranslate = require('next-translate');
-module.exports = nextTranslate({
-  webpack5: false,
+const withPreact = require('next-plugin-preact');
+module.exports = withPlugins([withPreact, nextTranslate], {
   reactStrictMode: false,
   publicRuntimeConfig: {
     INFURA_KEY: process.env.INFURA_KEY,
